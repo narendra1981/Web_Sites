87ed39d0de68052b424523ff3a360583cbe5fdb8
Azure User <azureuser@local>
2026-07-06 11:22:18 +0800

    Initial website commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const year = document.querySelector('.year');
+    if (year) {
+        year.textContent = new Date().getFullYear();
+    }
+});
